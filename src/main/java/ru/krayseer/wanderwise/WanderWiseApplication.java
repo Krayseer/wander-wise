1ea3aba814d121aebc0e45179dbf7abef763c658
Eugene <krayseer@gmail.com>
2024-01-31 19:43:43 +0300

    init commit

---
+++ src/main/java/ru/krayseer/wanderwise/WanderWiseApplication.java
@@ -0,0 +1,13 @@
+package ru.krayseer.wanderwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanderWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/krayseer/wanderwise/WanderWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krayseer.wanderwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
